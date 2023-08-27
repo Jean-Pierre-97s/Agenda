8d078d0e6b93a60dda8aafdb85c8de404eaa446e
Jean Pierre <jean_pierre_97@outlook.com>
2023-08-27 12:50:40 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    // Set up the view engine
+    app.useStaticAssets(path.join(process.cwd(), 'frontend', 'public')); // Assuming you have static assets in a 'public' folder
+    app.setBaseViewsDir(path.join(process.cwd(), 'frontend', 'views'));
+    app.setViewEngine('hbs'); // Use EJS as the view engine
+    
+  await app.listen(3000);
+}
+bootstrap();
